ce290225a1ca7a927a3635a831f15493e1cc4767
RAVR-NEW <ravr@netcompany.com>
2026-08-05 15:08:15 +0200

    removed unavailible builders from active builder config

--- active_builder_config.py
+++ active_builder_config.py
@@ -1,22 +1,16 @@
 from config import MY_DIRECTIONS, BrickDirection, Building, DIMENSION
 
 #import of builders
-from builders.empire_strikes_builder.builder import create_building as empire_strikes_builder
-#from builders.return_of_the_builder.builder import create_building as return_of_the_builder
-# from builders.new_hope_builder.builder import create_building as new_hope_builder
 # from builders.examples.naive_builder import naive_builder
 # from builders.examples.random_builder import random_builder
-# from builders.examples.greedy_builder.greedy_builder import greedy_builder as greedy_builder
+from builders.examples.greedy_builder.greedy_builder import greedy_builder as greedy_builder
 
 
 
 # CHANGE THIS LINE TO SELECT WHICH BUILDER TO USE
-ACTIVE_BUILDER = empire_strikes_builder
+ACTIVE_BUILDER = greedy_builder
 """
 Options: 
-    empire_strikes_builder, 
-    new_hope_builder, 
-    return_of_the_builder,
     naive_builder, 
     random_builder,
     greedy_builder,
